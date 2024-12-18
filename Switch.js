07005b9184aa22c4ee9447aa82c06359ce27bc27
Mkashif929 <ka2606212@gmail.com>
2024-12-18 15:27:53 +0500

    react start

---
+++ Switch.js
@@ -0,0 +1,15 @@
+var age = 30;
+switch (true) {
+    case (age >= 15 && age <= 25):
+        console.log("You are a Child")
+
+        break;
+
+    case (age >= 30 && age <= 40):
+        console.log("You are a Adult")
+        break;
+
+    default:
+        console.log("Your age is Invalid")
+
+}
